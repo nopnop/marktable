fc0ffde09719f24ea4f472c7b645aada8787ffc8
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-06-03 19:36:36 +0200

    test: Add base for acceptance test

--- test/acceptance/index.js
+++ test/acceptance/index.js
@@ -1,13 +1,57 @@
 var readFileSync = require('fs').readFileSync
-var join         = require('path').join
+var readdirSync  = require('fs').readdirSync
+var resolve      = require('path').resolve
+var humanize     = require('underscore.string').humanize
 
 
 exports.list = function() {
-
+  return readdirSync(__dirname)
+    .filter(function(filename) {
+      return /\.md$/.test(filename);
+    })
+    .map(function(filename) {
+      return {
+        should: 'Should ' + humanize(filename.slice(4,-3)),
+        read: function() {
+          return read(resolve(__dirname, filename))
+        }
+      }
+    })
 }
 
-exports.read = function (filename) {
-  var src    = readFileSync(join(__dirname, filename)).toString();
-  var result = [];
-  var stack  = [];
+function read(filepath) {
+  var lines  = readFileSync(resolve(__dirname, filepath))
+    .toString()
+    .split('\n');
+
+  var status = 0;
+  var result = {
+    comment   : '',
+    source   : [],
+    expected : []
+  }
+
+  lines.forEach(function(line) {
+    switch(status) {
+      case 0:
+        if(!/^>/.test(line)) return;
+        result.comment = line.slice(1).trim();
+        status = 1;
+        break;
+      case 1:
+        if(/^## SOURCE/.test(line))   return (status = 2);
+        break;
+      case 2:
+        if(/^## EXPECTED/.test(line)) return (status = 3);
+        result.source.push(line)
+        break;
+      case 3:
+        result.expected.push(line)
+    }
+  })
+
+  result.source   = result.source.join('\n');
+  result.expected = result.expected.join('\n');
+
+  return result;
 }
--- test/marktable.test.js
+++ test/marktable.test.js
@@ -2,7 +2,7 @@
 /* jshint undef: false, unused: false */
 
 var marktable = process.env.COVERAGE ? require('../lib-cov/marktable') : require('../lib/marktable')
-var expect    = require('expect')
+var expect    = require('expect.js')
 var rfile     = require('rfile')
 
 describe('marktable', function(){
