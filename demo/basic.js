8495634fd10338dc88949128d5603504c7d84e13
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-06-03 17:42:53 +0200

    doc: Add basic demo

---
+++ demo/basic.js
@@ -0,0 +1,69 @@
+var marktable = require('../lib/marktable'); // require('marktable')
+
+// Simple gfm table
+console.log(marktable([
+  '|Foo|Bar|',
+  '|-|',
+  '| 42 |',
+  '',
+].join('\n')))
+
+
+// Simple gfm table with alignements
+console.log(marktable([
+  '|Header 1| Header 2 |',
+  '|-:|:-:|',
+  '| 42 |',
+  '| | o |',
+  '',
+].join('\n')))
+
+
+// Colspan
+console.log(marktable([
+  '|Header 1| Header 2 |',
+  '|-|',
+  '| foo | bar |',
+  '| One cell ||',
+  '',
+].join('\n')))
+
+
+// Multiline
+console.log(marktable([
+  '|Long description|',
+  '|:-:|',
+  '| this |',
+  '| is |',
+  '| multiline |',
+  '| cell |',
+  '|-|',
+  '| Another cell |',
+  '',
+].join('\n')))
+
+/*
+Yield:
+| Foo | Bar |
+|-----|-----|
+| 42  |     |
+
+| Header 1 | Header 2 |
+|---------:|:--------:|
+|       42 |          |
+|          |     o    |
+
+| Header 1 | Header 2 |
+|----------|----------|
+| foo      | bar      |
+| One cell           ||
+
+| Long description |
+|:----------------:|
+|       this       |
+|        is        |
+|     multiline    |
+|       cell       |
+| ---------------- |
+|   Another cell   |
+*/
