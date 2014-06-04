8547b11f9765d5e0938fb23ae51f0d544e749123
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-06-04 09:47:55 +0200

    test: Add some acceptance tests

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -2,31 +2,43 @@ var readFileSync = require('fs').readFileSync
 var readdirSync  = require('fs').readdirSync
 var resolve      = require('path').resolve
 var humanize     = require('underscore.string').humanize
+var _            = require('underscore')
 
 
-exports.list = function() {
-  return readdirSync(__dirname)
-    .filter(function(filename) {
-      return /\.md$/.test(filename);
-    })
-    .map(function(filename) {
-      return {
-        should: 'Should ' + humanize(filename.slice(4,-3)),
-        read: function() {
-          return read(resolve(__dirname, filename))
-        }
+var tests = [];
+
+
+exports.add   = function(test) { tests.push(test) }
+exports.get   = function(id)   { return _.findWhere(tests, {id:id}) }
+exports.all   = function()     { return tests }
+exports.where = function(prop) { return _.where(tests, prop) }
+
+
+readdirSync(__dirname)
+  .forEach(function(filename) {
+    if(!/\.md$/.test(filename)) return;
+
+    var auto = /\.auto\.md$/.test(filename) ? true : false;
+
+    exports.add({
+      id  : parseInt(filename.slice(0, 3)),
+      spec: humanize(filename.slice(4, (auto ? -8 : -4))),
+      auto: auto,
+      read: function() {
+        return exports.read(resolve(__dirname, filename))
       }
     })
-}
+  })
 
-function read(filepath) {
+
+exports.read = function (filepath) {
   var lines  = readFileSync(resolve(__dirname, filepath))
     .toString()
     .split('\n');
 
   var status = 0;
   var result = {
-    comment   : '',
+    comment  : '',
     source   : [],
     expected : []
   }
--- test/marktable.test.js
+++ test/marktable.test.js
@@ -1,10 +1,22 @@
-
 /* jshint undef: false, unused: false */
 
-var marktable = process.env.COVERAGE ? require('../lib-cov/marktable') : require('../lib/marktable')
-var expect    = require('expect.js')
-var rfile     = require('rfile')
+var marktable  = process.env.COVERAGE ? require('../lib-cov/marktable') : require('../lib/marktable')
+var expect     = require('expect.js')
+var fixtures   = require('./fixtures/index')
+var lpad       = require('underscore.string').lpad
 
 describe('marktable', function(){
+
+
+  describe('Automatic tests', function() {
+    fixtures.where({auto:true})
+      .forEach(function(test) {
+        it(lpad(test.id,3,'0') + '. ' + test.spec,function() {
+          var fixture = test.read();
+          expect(marktable(fixture.source))
+            .to.eql(fixture.expected)
+        })
+      })
+  })
 
 })
