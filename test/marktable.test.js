86cb9c31e2a7afa03d714d6e80c2a209682b6532
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-06-22 22:58:26 +0200

    test: refactor test

--- test/marktable.test.js
+++ test/marktable.test.js
@@ -1,68 +1,17 @@
 /* jshint undef: false, unused: false */
 
 var marktable  = process.env.COVERAGE ? require('../lib-cov/marktable') : require('../lib/marktable')
+var _          = require('underscore')
 var expect     = require('expect.js')
 var fixtures   = require('./fixtures/index')
 var lpad       = require('underscore.string').lpad
 var debug      = require('debug')('marktable:test')
-var chokidar   = require('chokidar')
-var clc        = require('cli-color')
-
+var viewTable  = require('./tools/visualTableParseResult')
 var Document   = marktable.Document
 var Table      = marktable.Table
-
-// Use a watcher to kill test on fixture change
-var watcher    = new chokidar.FSWatcher();
-watcher.on('change', function(file) {
-  console.log('Change on ', file)
-  process.exit(0)
-})
-
-function debugMatchingInfo(infos) {
-  debug('TABLE range:[%s-%s] (%s %s %s %s)',
-    infos.offset,
-    infos.offsetEnd,
-    clc.red.inverse('caption'),
-    clc.blue.inverse('headers'),
-    clc.green.inverse('columns'),
-    clc.yellow.inverse('rows'),
-    '\n'+clc.red.inverse(infos.caption || '')
-    + clc.blue.inverse(infos.headers || '')
-    + clc.green.inverse(infos.columns || '')
-    + clc.yellow.inverse(infos.rows || '')
-  )
-}
-
-describe('marktable', function(){
+var Column     = marktable.Column
 
 
-
-
-  describe('Table', function() {
-    describe('#parse()', function() {
-      it('should parse one table in a source', function() {
-        var table  = new Table();
-        var src    = fixtures.get(101).read().source;
-        var result = table.parse(src);
-        debugMatchingInfo(result)
-        // console.log(table.toMarkdown())
-      })
-    })
-  })
-
-
-  describe('Automatic tests', function() {
-    fixtures.where({auto:true})
-      .forEach(function(test) {
-        watcher.add(test.file)
-        it(lpad(test.id,3,'0') + '. ' + test.spec,function() {
-          var fixture = test.read();
-          var result  = marktable(fixture.source);
-          debug(result);
-          expect(result.trim().split('\n'))
-            .to.eql(fixture.expected.trim().split('\n'))
-        })
-      })
-  })
+describe('marktable', function() {
 
 })
